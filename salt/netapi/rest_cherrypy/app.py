5f9f51a30250fd3aa77cda5ac8198369f753a4f3
Xiansong Wu （吴先松） <xiansongwu@trip.com>
2021-09-03 20:40:59 +0800

    token storage in multiple redis instances

--- salt/netapi/rest_cherrypy/app.py
+++ salt/netapi/rest_cherrypy/app.py
@@ -1156,10 +1156,7 @@ class LowDataAdapter:
         LowDataAdapter._cp_config.update({
             'tools.sessions.timeout': self.opts.get('session_redis_timeout'),
             'tools.sessions.storage_type': self.opts.get('session_storage_type'),
-            'tools.sessions.host': self.opts.get('session_redis_host'),
-            'tools.sessions.port': self.opts.get('session_redis_port'),
-            'tools.sessions.db': self.opts.get('session_redis_db'),
-            'tools.sessions.password': self.opts.get('session_redis_password', None)
+            'tools.sessions.redis_instances': self.opts.get('session_redis_instances')
         })
 
     def exec_lowstate(self, client=None, token=None):
--- salt/tokens/redis.py
+++ salt/tokens/redis.py
@@ -15,7 +15,7 @@ from salt.ext import six
 
 try:
     import redis
-
+    from concurrent.futures import ThreadPoolExecutor, wait
     HAS_REDIS = True
 except ImportError:
     HAS_REDIS = False
@@ -34,6 +34,91 @@ def __virtual__():
             "redis python client is not installed.",
         )
     return __virtualname__
+
+class RedisToken(object):
+
+    def __init__(self, opts):
+        self.redis_instances = opts.get('token_redis_instances')
+        self.expire_minutes = opts.get('token_redis_expire_minutes')
+        self.socket_timeout = opts.get('token_redis_socket_timeout', 60)
+        self.socket_connect_timeout = opts.get('token_redis_socket_connect_timeout', 5)
+
+        self.clients = []
+        for instance in self.redis_instances:
+            try:
+                client = redis.StrictRedis(
+                    host=instance['host'],
+                    port=instance['port'],
+                    db=instance['db'],
+                    password=instance.get('password', None),
+                    socket_timeout=self.socket_timeout,
+                    socket_connect_timeout=self.socket_connect_timeout,
+                    socket_keepalive=True)
+                self.clients.append(client)
+            except redis.exceptions.ConnectionError as err:
+                log.warning(
+                    "Failed to connect to redis at %s:%s - %s", instance['host'], instance['port'], err
+                )
+                self.clients = []
+                break
+
+
+    def _exist(self, client, token):
+        return bool(client.exists(token))
+
+    def exist(self, token):
+        for client in self.clients:
+            try:
+                ret = self._exist(client=client, token=token)
+                if ret is False:
+                    continue
+                return ret
+            except Exception as e:
+                log.error(str(e))
+                continue
+        return False
+
+    def _set(self, client, token, data):
+        client.setex(token, self.expire_minutes * 60, data)
+
+    def set(self, token, data):
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(fn=self._set, client=client, token=token, data=data) for client in self.clients]
+            wait(futures)
+
+    def _get(self, client, token):
+        return client.get(token)
+
+    def get(self, token):
+        for client in self.clients:
+            try:
+                ret = self._get(client=client, token=token)
+                if ret:
+                    return ret
+            except Exception as e:
+                log.error(str(e))
+                continue
+        return None
+
+    def delete(self, token):
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(fn=client.delete, token=token) for client in self.clients]
+            wait(futures)
+
+    def _keys(self, client):
+        return client.keys()
+
+    def keys(self):
+        for client in self.clients:
+            try:
+                ret = self._keys(client=client)
+                if ret:
+                    return ret
+            except Exception as e:
+                log.error(str(e))
+                continue
+        return None
+
 
 
 def _redis_client(opts):
@@ -69,8 +154,9 @@ def mk_token(opts, tdata):
     :param tdata: Token data to be stored with 'token' attribute of this dict set to the token.
     :returns: tdata with token if successful. Empty dict if failed.
     """
-    redis_client = _redis_client(opts)
-    if not redis_client:
+
+    redis_client = RedisToken(opts)
+    if not redis_client.clients:
         return {}
     hash_type = getattr(hashlib, opts.get("hash_type", "md5"))
     tok = six.text_type(hash_type(os.urandom(512)).hexdigest())
@@ -86,7 +172,7 @@ def mk_token(opts, tdata):
     serial = salt.payload.Serial(opts)
     try:
         redis_timeout = opts.get('token_redis_timeout')
-        redis_client.setex(tok, redis_timeout, serial.dumps(tdata))
+        redis_client.set(tok, serial.dumps(tdata))
     except Exception as err:  # pylint: disable=broad-except
         log.warning(
             "Authentication failure: cannot save token %s to redis: %s", tok, err
@@ -103,8 +189,8 @@ def get_token(opts, tok):
     :param tok: Token value to get
     :returns: Token data if successful. Empty dict if failed.
     """
-    redis_client = _redis_client(opts)
-    if not redis_client:
+    redis_client = RedisToken(opts)
+    if not redis_client.clients:
         return {}
     serial = salt.payload.Serial(opts)
     try:
@@ -125,8 +211,8 @@ def rm_token(opts, tok):
     :param tok: Token to remove
     :returns: Empty dict if successful. None if failed.
     """
-    redis_client = _redis_client(opts)
-    if not redis_client:
+    redis_client = RedisToken(opts)
+    if not redis_client.clients:
         return
     try:
         redis_client.delete(tok)
@@ -143,8 +229,8 @@ def list_tokens(opts):
     :returns: List of dicts (tokens)
     """
     ret = []
-    redis_client = _redis_client(opts)
-    if not redis_client:
+    redis_client = RedisToken(opts)
+    if not redis_client.clients:
         return []
     serial = salt.payload.Serial(opts)
     try:
